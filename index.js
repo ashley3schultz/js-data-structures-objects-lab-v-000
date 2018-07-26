059b842b193f17d0af67112dfd3d7c3958b61c51
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-26 03:04:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 const driver = { foo: 'bar' }
 
-function updateDriverWithKeyAndValue(driver, key, value) {
+function updateDriverWithKeyAndValue(obj, key, value) {
   const new = { ...driver }
   newobj[key] = value
   return newobj
