e13d652847029c0b682aa52555a8882d80a3f9e3
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-26 03:07:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function updateDriverWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
-  return obj[key]: = value
+  return obj[key] = value
 }
